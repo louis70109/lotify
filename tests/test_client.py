7c1c0b3266ac14882dd7dcbfea668297b7f0ef12
NiJia <louis70109@gmail.com>
2020-04-25 20:01:33 +0800

    test: add setup

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,11 +1,50 @@
 import unittest
+from urllib.parse import urlencode
+
+import responses
+
 from line_notify.client import Client
 
 
 class TestClient(unittest.TestCase):
-    def test_get_auth_link(self):
+    def setUp(self):
+        self.tested = Client(
+            client_id='QxUxF..........i51eITH',
+            client_secret='fmtJS3GOVTPn4....................bFcIvJf1jo',
+            redirect_uri='http://localhost:5000/notify')
 
-        client = Client(client_id='QxUxF..........i51eITH', client_secret='fmtJS3GOVTPn4....................bFcIvJf1jo',
-                        redirect_uri='http://localhost:5000/notify')
-        result = client.get_auth_link('foo')
-        self.assertEqual(result, 'https://notify-bot.line.me/oauth/authorize?scope=notify&response_type=code&client_id=QxUxF..........i51eITH&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fnotify&state=foo')
+        self.bot_origin = "https://notify-bot.line.me"
+        self.api_origin = "https://notify-api.line.me"
+
+    def test_get_auth_link(self):
+        result = self.tested.get_auth_link('foo')
+        expected_query_string = {
+            'scope': 'notify',
+            'response_type': 'code',
+            'client_id': 'QxUxF..........i51eITH',
+            'redirect_uri': 'http://localhost:5000/notify',
+            'state': 'foo'
+        }
+        self.assertEqual(
+            result,
+            '{url}/oauth/authorize?{query_string}'.format(
+                url=self.bot_origin,
+                query_string=urlencode(expected_query_string)
+            )
+        )
+
+    @responses.activate
+    def test_get_access_token(self):
+        responses.add(
+            responses.POST,
+            'https://notify-bot.line.me/oauth/token',
+            json={
+                'access_token': 'access_token_foo'
+            },
+            status=200
+        )
+
+        result = self.tested.get_access_token('foo')
+        request = responses.calls[0].request
+        self.assertEqual('POST', request.method)
+        self.assertEqual('access_token_foo', result)
