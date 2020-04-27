3fd99e5e941245a53bf40fb5169752ad04ba4295
NiJia <louis70109@gmail.com>
2020-04-28 00:17:29 +0800

    Updated: environmet config settings

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-import re
-from os import path
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+import re
+import sys
+
+from os import path
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
 version_file = path.join(
     path.dirname(__file__),
     '__version__.py'
 )
+
 with open(version_file, 'r') as fp:
     m = re.search(
         r"^__version__ = ['\"]([^'\"]*)['\"]",
@@ -15,30 +21,63 @@ with open(version_file, 'r') as fp:
     )
     version = m.groups(1)[0]
 
+
+def _requirements():
+    with open('requirements.txt', 'r') as fd:
+        return [name.strip() for name in fd.readlines()]
+
+
+def _requirements_test():
+    with open('requirements-dev.txt', 'r') as fd:
+        return [name.strip() for name in fd.readlines()]
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
 setup(
     name='lotify',
     version=version,
     description='Using Line Notify more easily',
     url='https://github.com/louis70109/line-notify',
     author='NiJia Lin',
+    author_email='louis70109@gmail.com',
+    maintainer="NiJia Lin",
+    maintainer_email="louis70109@gmail.com",
     long_description=open('README.rst').read().strip(),
     long_description_content_type="text/x-rst",
-    author_email='louis70109@gmail.com',
     keywords='line notify python lotify',
     license='MIT',
-    platforms='any',
     packages=find_packages(exclude=['tests']),
-    install_requires=[
-        'requests==2.22.0'
-    ],
+    install_requires=_requirements(),
+    tests_require=_requirements_test(),
+    cmdclass={'test': PyTest},
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-    zip_safe=False,
     project_urls={
-        'Bug Reports': 'https://github.com/louis70109/line-notify/issues',
-        'Source': 'https://github.com/louis70109/line-notify',
+        'Bug Reports': 'https://github.com/louis70109/lotify/issues',
+        'Source': 'https://github.com/louis70109/lotify',
     },
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7'
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development"
     ],
 )
